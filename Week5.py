bfd6fa0d984d7521b4652bb699ee22ad190f2995
Khasan <kjurakulov@students.wiut.uz>
2022-10-11 11:56:47 +0500

    Task 5

--- Week5.py
+++ Week5.py
@@ -1,3 +1,11 @@
-color_list = ["Red", "Green", "White", "Black"]
-print(color_list[0])
-print(color_list[-1])
+n = 10 
+for i in range(n):
+    for j in range(i):
+        print("* ", end='')
+    print('')
+
+for i in range(n,0,-1):
+    for j in range(i):
+        print("* ", end='')
+    print('')
+
